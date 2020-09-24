d3c9f327deb5a442c2eacc61da8a96fb71276d00
Sophie M <sophie.maurice.etu@protonmail.com>
2020-09-24 23:00:58 +0200

    update ex14

--- ex14/TEST.py
+++ ex14/TEST.py
@@ -5,7 +5,9 @@ import unittest
 class Test(unittest.TestCase):
 
     def TestValue(self):
-      self.assertEqual(main.getY(), "set['s', 'b', 'd']")
+        self.assertEqual(main.objet.y, 28)
+        self.assertEqual(main.objet.x, 23)
+        self.assertEqual(main.objet.affiche(), "(28, 42)")
 
 
 if __name__ == '__main__':
--- ex14/main.py
+++ ex14/main.py
@@ -2,98 +2,18 @@
 import math
 
 
-def getX():
-    X = {'a', 'b', 'c', 'd'}
-    return X
+class MaClasse:
+    x=23
+    y = x+5
 
-
-def getY():
-    Y = {'s', 'b', 'd'}
-    return Y
-
-
-def setsfunction(a, b):
-    try:
-        X = a
-        Y = b
-        listX = []
-        listY = []
-        for i in X:
-            listX.append(i)
-        for i in Y:
-            listY.append(i)
-        listX = sorted(listX)
-        print("X : {'" + str(listX[0]) + "','" + str(listX[1]) + "', '" + str(listX[2]) + "', '" + str(listX[3]) + "'}")
-        print("Y : {'" + str(listY[0]) + "','" + str(listY[1]) + "', '" + str(listY[2]) + "'}")
-    except Exception:
-        raise Exception
-
-
-def appartenance(a, b):
-    char = a
-    try:
-        if b == 'X':
-            f = getX()
-        elif b == 'Y':
-            f = getY()
-        else:
-            raise ValueError
-        expression = char in f
-        print(str(char) + " in " + str(b) + " : " + str(expression))
-    except ValueError:
-        print("ERROR")
-
-
-def operation(a, b, c):
-    type_operation = c
-    res = {}
-    #1er Ensemble
-    if a == 'X':
-       ensemble1 = getX()
-    elif a == 'Y':
-        ensemble1 = getY()
-    else:
-        raise ValueError
-    #2eme Ensemble
-    if b == 'X':
-       ensemble2 = getX()
-    elif b == 'Y':
-        ensemble2 = getY()
-    else:
-        raise ValueError
-    #Operation
-    if type_operation == '-':
-        res = ensemble1 - ensemble2
-    elif type_operation == 'union':
-        res = ensemble1.union(ensemble2)
-    elif type_operation == 'inter':
-        res = ensemble1.intersection(ensemble2)
-
-    listRes = []
-    for i in res:
-        listRes.append(i)
-    tailleRes = len(res)
-
-    #Affichage
-    msg = str(a) + " " + str(type_operation) + " " + str(b) + " : {"
-    i = 0
-    while i < tailleRes:
-        msg = msg+"'" + str(listRes[i]) + "'"
-        if i < (tailleRes - 1):
-            msg = msg+", "
-        i = i + 1
-    msg += "}"
-    print(msg)
+    def affiche(self):
+        z=42
+        print("("+str(self.y)+", "+str(z)+")")
 
 
 if __name__ == '__main__':
     try:
-        setsfunction(getX(), getY())
-        appartenance('c', 'X')
-        appartenance('a', 'Y')
-        operation('X', 'Y', '-')
-        operation('Y', 'X', '-')
-        operation('X', 'Y', 'union')
-        operation('X', 'Y', 'inter')
+        objet = MaClasse()
+        objet.affiche()
     except Exception:
         print("ERROR")
