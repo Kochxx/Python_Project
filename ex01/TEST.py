97b0f92f80948cac0fed7bc07fccc49b0a065680
Sophie M <sophie.maurice.etu@protonmail.com>
2020-09-25 11:53:07 +0200

    update comments and test from ex01

--- ex01/TEST.py
+++ ex01/TEST.py
@@ -4,13 +4,19 @@ import random
 
 
 class Test(unittest.TestCase):
+    #teste la valeur 0 pour le dénominateur, puis pour le numérateur
     def testZeroDivision(self):
         print("Zero Division Test")
         self.assertEqual(main.handling(1, 0.0), 0.0)
-        with self.assertRaises(ZeroDivisionError):
-            main.handling(0.0, 1)
+        self.assertRaises(ZeroDivisionError, main.handling(0.0, 1.0))
 
-    def
+    #teste la valeur obtenue par la fonction handling
+    def testValueVitesse(self):
+        print("Value Vitesse Test")
+        self.assertEqual(float('{:.1f}'.format(main.handling(7.8, 4.0))), 0.5)
+        self.assertEqual(float('{:.1f}'.format(main.handling(9.0, 3))), 0.3)
+        self.assertEqual(float('{:.1f}'.format(main.handling(2, 10.0))), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
--- ex01/main.py
+++ ex01/main.py
@@ -1,28 +1,39 @@
 # -*- coding: utf-8 -*-
+import sys
 
+
+#function that calculates the speed from the time and distance passed as a parameter
 def handling(a, b):
-    temps = a
+    time = a
     dist = b
     try:
-        v = dist/temps
+        #division
+        v = dist/time
     except ZeroDivisionError:
         print("ERROR")
     else:
         return v
 
-
+#main function
 if __name__ == '__main__':
+    #open file TEST.py
     try:
-        file = open("INPUT.TXT", "r")
+        nameFile = sys.argv[1]
+        file = open(nameFile, "r")
     except FileNotFoundError:
         print("ERROR")
     else:
+        #reading the file
         for lines in file.readlines():
             try:
+                #separates arguments from a line
                 tab = lines.split(";")
+                #checks the number of arguments
                 if len(tab) != 2:
                     raise Exception
+                #retrieves the time argument from the line
                 temps = float(tab[0])
+                #retrieves the distance argument from the line
                 dist = float(tab[1])
 
             except Exception:
@@ -30,7 +41,9 @@ if __name__ == '__main__':
 
             else:
                 try:
+                    #print the speed separates arguments from a line
                     print('{:.1f}'.format(handling(temps, dist)))
                 except Exception:
                     print("ERROR")
+        #closed file INPUT.txt
         file.close()
