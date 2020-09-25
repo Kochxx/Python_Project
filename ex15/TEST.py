31d8554b432bb5d9b9e753c5b61cbf215f6c963b
Kochxx <sophie.maurice.etu@protonmail.com>
2020-09-25 19:25:33 +0200

    create ex15

---
+++ ex15/TEST.py
@@ -0,0 +1,13 @@
+import main
+import unittest
+import random
+
+
+class Test(unittest.TestCase):
+
+    def testClass(self):
+        print("Test")
+
+
+if __name__ == '__main__':
+    unittest.main()
