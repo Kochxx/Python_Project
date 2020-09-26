57e251b73d08d4618e1a9e6168f47360ce917f39
Kochxx <sophie.maurice.etu@protonmail.com>
2020-09-26 17:49:21 +0200

    update comments and test ex16

--- ex16/TEST.py
+++ ex16/TEST.py
@@ -21,6 +21,15 @@ class Test(unittest.TestCase):
         self.assertEqual(vecteur.x, 0)
         self.assertEqual(vecteur.y, 0)
 
+    # Test to checks the value of a vector with add function
+    def testAddVectors(self):
+        print("Test Add Vectors")
+        vecteur1 = main.Vecteur2D(2.1, 3.0)
+        vecteur2 = main.Vecteur2D(12.3, 54.1, 2)
+        vecteur1.__add__(vecteur2)
+        self.assertEqual(vecteur1.x, 14.4)
+        self.assertEqual(vecteur1.y, 57.1)
+
 
 if __name__ == '__main__':
     unittest.main()
--- ex16/main.py
+++ ex16/main.py
@@ -7,9 +7,31 @@ import sys
 class Vecteur2D:
 
     # init class
-    def __init__(self, cX = 0.0, cY = 0.0):
+    def __init__(self, cX=0.0, cY=0.0, n=0):
+        # x coordinate
         self.x = cX
+        # y coordinate
         self.y = cY
+        # number to define the type of vector ( the vector 1, 2 or the somme of the add function)
+        self.num = n
+
+    # displays vector
+    def affichage(self):
+        # print the vector like vector 1
+        if self.num == 0:
+            print("v1 : (x=" + str(self.x) + ", y=" + str(self.y) + ")")
+        # print the vector like vector 'somme'
+        elif self.num == -1:
+            print("somme : (x=" + str(self.x) + ", y=" + str(self.y) + ")")
+        else:
+            # print the vector like other vector
+            print("v" + str(self.num) + " : (x=" + str(self.x) + ", y=" + str(self.y) + ")")
+
+    # function that adds two vectors
+    def __add__(self, v2):
+        self.x = self.x + v2.x
+        self.y = self.y + v2.y
+        self.num = -1
 
 
 # main function
@@ -19,35 +41,48 @@ if __name__ == '__main__':
         nameFile = sys.argv[1]
         file = open(nameFile, "r")
     except FileNotFoundError:
-        print("ERROR")
+        print("ERROR1")
     else:
         # reading the file
         for lines in file.readlines():
             try:
-                # separates arguments from a line
-                tab = lines.split(";")
-                # checks the number of arguments
+                # cuts the line in half starting from the symbol ','
+                tab = lines.split(",")
                 if len(tab) != 2:
                     raise Exception
-                # retrieves the x argument from the line
-                x = float(tab[0])
-                # retrieves the y argument from the line
-                y = float(tab[1])
+                # cuts the line in half starting from the symbol ';'
+                tab1 = tab[0].split(";")
+                if len(tab1) != 2:
+                    raise Exception
+                # cuts the line in half starting from the symbol ';'
+                tab2 = tab[1].split(";")
+                if len(tab2) != 2:
+                    raise Exception
+                # defines the x coordinate of vector 1
+                x1 = float(tab1[0])
+                # defines the y coordinate of vector 1
+                y1 = float(tab1[1])
+                # defines the x coordinate of vector 2
+                x2 = float(tab2[0])
+                # defines the y coordinate of vector 2
+                y2 = float(tab2[1])
+
+                # instantiation vector 1
+                vecteur1 = Vecteur2D(x1, y1)
+                # instantiation vector 1
+                vecteur2 = Vecteur2D(x2, y2, 2)
+                # display vector 1
+                vecteur1.affichage()
+                # display vector 2
+                vecteur2.affichage()
+                # calculate add
+                vecteur1.__add__(vecteur2)
+                # display vector somme
+                vecteur1.affichage()
 
             except Exception:
                 print("ERROR")
 
-            else:
-                try:
-                    # instantiation without paramaters
-                    vecteur = Vecteur2D()
-                    # instantiation with paramaters
-                    vecteur2 = Vecteur2D(x, y)
-                    print("par défaut : (x=" + str(vecteur.x) + ", y=" + str(vecteur.y) + ")")
-                    print("instance : (x=" + str(vecteur2.x) + ", y=" + str(vecteur2.y) + ")")
-
-                except Exception:
-                    print("ERROR")
-
-                # close file
-                file.close()
+            # close file
+            file.close()
+#
