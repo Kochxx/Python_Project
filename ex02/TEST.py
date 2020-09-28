cc45c235080482e5b04e4307034749aefa7dd401
Kochxx <sophie.maurice.etu@protonmail.com>
2020-09-28 13:31:49 +0200

    edit comments ex01/ex02

--- ex02/TEST.py
+++ ex02/TEST.py
@@ -4,17 +4,27 @@ import random
 
 
 class Test(unittest.TestCase):
-    # test the value error
+    """
+        ..class:: Test class ex02
+    """
+
     def testRaiseValueTypeError(self):
+        """
+            ..function:: Test the value error
+        """
         print("testRaiseValueTypeError")
-        self.assertRaises(ValueError, main.squareRoot, -8.5)
+        test1 = main.ex02()
+        self.assertRaises(ValueError, test1.squareRoot, -8.5)
         print("Incorrect value => expected value none negative")
 
-    # test value
     def testValue(self):
+        """
+            ..function:: Test the value
+        """
         print("testValue")
-        self.assertEqual(main.squareRoot(9), 3)
-        self.assertEqual(main.squareRoot(25), 5)
+        test2 = main.ex02()
+        self.assertEqual(test2.squareRoot(9), 3)
+        self.assertEqual(test2.squareRoot(25), 5)
 
 
 if __name__ == '__main__':
