3d46d2e3549292789096395c18003cd82b977ee7
rosalieperee <roseallie92@gmail.com>
2020-10-02 19:04:29 +0200

    corresting typoes

--- ex09/main.py
+++ ex09/main.py
@@ -76,7 +76,7 @@ class ex09:
         ..function: Function that returns different type of sub-list
 
         :param firstElement: the first element to define the sub-list
-        :param secundElement: the other element to define the sub-list
+        :param secondElement: the other element to define the sub-list
 
         :return res: the sub-list defined
         """
@@ -84,21 +84,21 @@ class ex09:
             l = self.liste
             res = ""
             if isinstance(firstElement, int):
-                if isinstance(secundElement, int):
-                    res = l[int(firstElement):int(secundElement)]
-                elif secundElement == 'oui':
+                if isinstance(secondElement, int):
+                    res = l[int(firstElement):int(secondElement)]
+                elif secondElement == 'oui':
                     res = l[int(firstElement):]
                 else:
                     raise Exception
             elif firstElement == 'oui':
-                if isinstance(secundElement, int):
-                    res = l[:int(secundElement)]
-                elif secundElement == 'oui':
+                if isinstance(secondElement, int):
+                    res = l[:int(secondElement)]
+                elif secondElement == 'oui':
                     res = l[::]
                 else:
                     raise Exception
             else:
-                res = l[int(firstElement):int(secundElement)]
+                res = l[int(firstElement):int(secondElement)]
             return res
         except Exception:
             print("ERROR")
